4cf4684223abe02642731f6fc6c26ff23e9a2bbd
Taimoor Tariq <taimoorhero15@gmail.com>
2020-09-21 00:20:47 -0400

    Create RoleMembers.js

---
+++ modules/RoleMembers.js
@@ -0,0 +1,42 @@
+const jsonexport = require('jsonexport');
+const fs = require('fs');
+
+exports.size = (msg, args) => {
+    let roleID = args[0].split("<@&")[1];
+    roleID = roleID.slice(0, -1);
+
+    let role = msg.guild.roles.get(roleID);
+
+    message.channel.send(`**${role.name}** has **${role.members.size}** members`);
+}
+
+exports.members = (msg, args) => {
+    let roleID = args[0].split("<@&")[1];
+    roleID = roleID.slice(0, -1);
+
+    let role = msg.guild.roles.get(roleID);
+
+    let rMembers = [];
+    
+    role.members.forEach(m => {
+        let pushing = {
+            "ID": `${m.id}`,
+            "Username": m.user.tag
+        }
+        rMembers.push(pushing);
+    });
+
+    jsonexport(rMembers, function(err, csv){
+        if (err) return console.error(err);
+        // msg.channel.send(csv);
+        fs.writeFile('./roleList.csv', csv, function (err) {
+            if (err) throw err;
+            msg.channel.send("Here is the list of members.", {
+                files: [
+                    "./roleList.csv"
+                ]
+            });
+            console.log('Saved!');
+        });
+    });
+}
