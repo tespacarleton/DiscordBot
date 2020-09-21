bd720bcd9997a06af246d04de3662c0985b9b06c
Taimoor Tariq <taimoorhero15@gmail.com>
2020-09-21 00:52:30 -0400

    No args for !rolemembers now gets everyone

--- modules/RoleMembers.js
+++ modules/RoleMembers.js
@@ -11,32 +11,68 @@ exports.size = (msg, args) => {
 }
 
 exports.members = (msg, args) => {
-    let roleID = args[0].split("<@&")[1];
-    roleID = roleID.slice(0, -1);
-
-    let role = msg.guild.roles.get(roleID);
-
-    let rMembers = [];
+    if (args.length != 0) {
+        let roleID = args[0].split("<@&")[1];
+        roleID = roleID.slice(0, -1);
     
-    role.members.forEach(m => {
-        let pushing = {
-            "ID": `${m.id}`,
-            "Username": m.user.tag
-        }
-        rMembers.push(pushing);
-    });
-
-    jsonexport(rMembers, function(err, csv){
-        if (err) return console.error(err);
-        // msg.channel.send(csv);
-        fs.writeFile('./roleList.csv', csv, function (err) {
-            if (err) throw err;
-            msg.channel.send("Here is the list of members.", {
-                files: [
-                    "./roleList.csv"
-                ]
-            });
-            console.log('Saved!');
+        let role = msg.guild.roles.get(roleID);
+    
+        let rMembers = [];
+        
+        role.members.forEach(m => {
+            let pushing = {
+                "ID": `${m.id}`,
+                "Username": m.user.tag
+            }
+            rMembers.push(pushing);
         });
-    });
+    
+        jsonexport(rMembers, function(err, csv){
+            if (err) return console.error(err);
+            // msg.channel.send(csv);
+            fs.writeFile('./roleList.csv', csv, function (err) {
+                if (err) throw err;
+                msg.channel.send("Here is the list of members.", {
+                    files: [
+                        "./roleList.csv"
+                    ]
+                });
+                console.log('Saved!');
+            });
+        });
+    }
+    else {
+        let rMembers = [];
+        msg.guild.members.forEach(m => {
+            let roles = "";
+
+            m.roles.forEach(r => {
+                roles = `${roles} ${r.name},`
+            })
+
+            roles = roles.slice(0, -1);
+            roles.replace("@everyone, ", "");
+            
+            let pushing = {
+                "ID": `${m.id}`,
+                "Username": m.user.tag,
+                "Roles": roles
+            }
+            rMembers.push(pushing);
+        });
+
+        jsonexport(rMembers, function(err, csv){
+            if (err) return console.error(err);
+            // msg.channel.send(csv);
+            fs.writeFile('./roleList.csv', csv, function (err) {
+                if (err) throw err;
+                msg.channel.send("Here is the list of members.", {
+                    files: [
+                        "./roleList.csv"
+                    ]
+                });
+                console.log('Saved!');
+            });
+        });
+    }
 }
